8bd785306b7264b827b0285643edc798727365d6
unknown <wardahnabilah33@gmail.com>
2023-07-23 22:23:21 +0700

    Create TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-[18rem] p-4 border-white bg-gradient-to-r from-[#e944608a] to-[#A5D7E8] rounded-lg">
+        <h2 class="mb-2 text-xl text-[#090f1b] font-bold">Read a book</h2>
+        <p class="text-sm">Deadline: 22 Aug 2023 02.43 AM</p>
+    </div>
+</template>
