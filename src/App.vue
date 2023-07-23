12f333a228206cc4c3c91a73aa9a9fb2ef1d1dcb
unknown <wardahnabilah33@gmail.com>
2023-07-23 22:45:45 +0700

    Fix styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { ref } from 'vue';
+  import TodoItem from './components/TodoItem.vue'
 
   const today = new Date()
   const day = today.getDate() > 10 ? today.getDate() : '0' + today.getDate()
@@ -14,9 +15,9 @@
 </script>
 
 <template>
-  <div class="container">
+  <div class="container mx-auto">
     <h1 class="text-center text-3xl font-bold text-white mb-5">Todo List</h1>
-    <div class="grid max-w-[90%] md:max-w-[25%] mx-auto">
+    <div class="grid max-w-[90%] md:max-w-[25%] mx-auto mb-12">
       <input class="px-5 py-2 mb-3 bg-transparent border outline-none focus:border-[#4d71d3] rounded-full text-white" type="text" placeholder="What to do...">
       <label class="mb-2 block text-white" for="deadline">Deadline: </label>
       <div class="flex mb-6 ">
@@ -24,6 +25,9 @@
         <input class="px-5 py-1.5 bg-transparent text-white border outline-none focus:border-[#4d71d3] rounded-full" type="time" v-model="time">
       </div>
       <button class="p-1.5 bg-[#e9445f] hover:bg-[#8b2738] shadow rounded-full text-white font-bold">ADD</button>
-    </div>  
+    </div>
+    <TodoItem />  
   </div>
+
+  
 </template>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-    <div class="w-[18rem] p-4 border-white bg-gradient-to-r from-[#e944608a] to-[#A5D7E8] rounded-lg">
+    <div class="w-[18rem] p-4 relative border-white bg-gradient-to-r from-[#e944608a] to-[#A5D7E8] rounded-lg">
         <h2 class="mb-2 text-xl text-[#090f1b] font-bold">Read a book</h2>
         <p class="text-sm">Deadline: 22 Aug 2023 02.43 AM</p>
+        <div class="absolute top-2.5 right-2.5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"></path></svg>
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"></path><path d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"></path></svg> -->
+        </div>
+        <svg class="absolute bottom-2.5 right-2.5 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgb(116, 18, 18);transform: ;msFilter:;"><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm4 12H8v-9h2v9zm6 0h-2v-9h2v9zm.618-15L15 2H9L7.382 4H3v2h18V4z"></path></svg>
     </div>
 </template>
